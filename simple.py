de0bd8f810e10094a64cc7b772d2dd8785526fea
bodhibrdr <julie.gumerman@gmail.com>
2020-03-24 13:36:56 -0600

    funky comma error fixed

--- simple.py
+++ simple.py
@@ -34,36 +34,54 @@ register = [0] * 8
 pc = 0
 running = True
 
-while running:
-    #REPL: read eval print loop
-    command = memory[pc]
+def load_memory(filename):
+    try:
+        with open(filename) as f:
+            for line in f:
+                print(line)
+                #ignore comments
+                comment_split = line.split("#")
+                #strip out white space
+                num = comment_split.strip() #strip() removes white space
+                #ignore blank lines
+                if num == "":
+                    continue
+            
+                val = int(num)
+    except FileNotFoundError:
+        print("File not found")
+        sys.exit(2)
 
-    if command == PRINT_BEEJ:
-        print("BEEJ")
-        pc += 1
-    elif command == HALT:
-        running = False
-        pc += 1
-    elif command == PRINT_NUM:
-        num = memory[pc + 1]
-        print(num)
-        pc += 2
-    elif command == SAVE:
-        num = memory[pc + 1]
-        reg = memory[pc + 2]
-        register[reg] = num
-        pc += 3
-    elif command == PRINT_REGISTER:
-        reg = memory[pc + 1]
-        print(register[reg])
-        pc += 2
+# while running:
+#     #REPL: read eval print loop
+#     command = memory[pc]
 
-    elif command == ADD:
-        reg_a = memory[pc + 1]
-        reg_b = memory[pc + 2]
-        register[reg_a] += register[reg_b]
-        pc+=3
-    else:
-        print(f"Unknown instruction: {command}")
-        sys.exit(1)
+#     if command == PRINT_BEEJ:
+#         print("BEEJ")
+#         pc += 1
+#     elif command == HALT:
+#         running = False
+#         pc += 1
+#     elif command == PRINT_NUM:
+#         num = memory[pc + 1]
+#         print(num)
+#         pc += 2
+#     elif command == SAVE:
+#         num = memory[pc + 1]
+#         reg = memory[pc + 2]
+#         register[reg] = num
+#         pc += 3
+#     elif command == PRINT_REGISTER:
+#         reg = memory[pc + 1]
+#         print(register[reg])
+#         pc += 2
+
+#     elif command == ADD:
+#         reg_a = memory[pc + 1]
+#         reg_b = memory[pc + 2]
+#         register[reg_a] += register[reg_b]
+#         pc+=3
+#     else:
+#         print(f"Unknown instruction: {command}")
+#         sys.exit(1)
     
